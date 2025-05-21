81db95d437d748455023a83ed1d896545ff3f074
Ivan Concon <iconcon@myseneca.ca>
2025-05-21 13:02:33 -0400

    added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Ivan Concon
+# Author ID: 137013231
+# Date Created: 2025/05/20
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
