3a4699545cbe099fd32b8b6f3b3176bd2aba6762
Ivan Concon <iconcon@myseneca.ca>
2025-05-21 13:02:21 -0400

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Ivan Concon
+# Author ID: 137013231
+# Date Created: 2025/05/20
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
